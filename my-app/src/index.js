709611e63419e914f1d20d4ed0eaa4844363bdcd
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-07-30 10:25:12 +0200

    lifting state up; immutability of state

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: null
+  //   };
+  // }
 
   render() {
     return (
@@ -16,19 +16,38 @@ class Square extends React.Component {
         className="square"
         onClick={() => {
           /* alert("clicked"); */
-          this.setState({ value: "X" });
-          console.log("Square " + this.state.value + " clicked"); //will display the old valuse (probably because it is updates only when rendered! )
+          this.props.onClick();
+          console.log("Square " + this.props.value + " clicked"); //will display the old valuse (probably because it is updates only when rendered! )
         }}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i) {
+    //we go here witth the immutability approach, by rewriting instead of mutating the state. This allows us several benefits with the most important one being able to use `React.PureComponent` instead of writing `shouldComponentUpdate()`:
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
