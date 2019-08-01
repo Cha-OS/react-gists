682d8399ef0b712a37967871ac38b89b5c18ddbd
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-08-01 10:12:26 +0200

    NEW GIST: form gist added

---
+++ form/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import NameForm from "./NameForm.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">React Forms Gist</header>
+
+      <NameForm />
+    </div>
+  );
+}
+
+export default App;
